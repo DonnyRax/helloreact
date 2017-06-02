019b44f789eb48a87ddece56a220f719b9ecebad
Rakesh Chotalia <Rax@Rakeshs-MacBook-Pro.local>
2017-06-02 20:39:14 +0100

    re

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 module.exports = {
-    entry: './public/app.js',
+    entry: './public/app.jsx',
     output: {
         path: __dirname,
         filename: './public/bundle.js'
     },
     resolve: {
         extentions: ['','.js','.jsx']
+    },
+    module: {
+        loader: {
+            loader: 'babel',
+            query: {
+                presets: ['es2015','react']
+            },
+            test: '/\.jsx?$/',
+            exclude: /(node_modules|bower_compnents)/
+        }
     }
 };
